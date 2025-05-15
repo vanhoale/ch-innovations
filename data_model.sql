fe2806c36e759794ae8c257f5902759dbe32bcc7
Hoa Le <hoa.le@icf.com>
2025-05-15 18:59:59 -0500

    Update data_model.sql

--- data_model.sql
+++ data_model.sql
@@ -38,11 +38,11 @@ insert into person(name, email) values ('Daniel', 'eaniel@example.com');
 
 insert into task(name, description) values('task1', 'monthly, end after 12 occurrences');
 insert into task(name, description) values('task2', 'one occurrence');
-insert into task(name, description) values('task2', 'daily, end after 30 occurrences');
+insert into task(name, description) values('task3', 'daily, end after 30 occurrences');
 
 insert into task_occurence(task_id, recurrence, start_date, end_date, status) values (1, 'Monthly', now(), now() + interval '1 year', 'Not Started');
 insert into task_occurence(task_id, recurrence, start_date, end_date, status) values (2, null, now() + interval '1 day', now() + interval '1 day', 'Not Started');
-insert into task_occurence(task_id, recurrence, start_date, end_date, status) values (2, null, now() - interval '1 day', now() + interval '29 day', 'In Progress');
+insert into task_occurence(task_id, recurrence, start_date, end_date, status) values (3, 'Daily', now() - interval '1 day', now() + interval '29 day', 'In Progress');
 
 insert into task_assigment(task_id, person_id) values (1,1);
 insert into task_assigment(task_id, person_id) values (1,2);
